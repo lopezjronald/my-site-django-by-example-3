4a4b15b13b3899598d66291f9e5dd850acef8900
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2021-09-19 01:55:30 -0700

    added tagging feature to blog site

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishManager(models.Manager):
@@ -34,6 +35,7 @@ class Post(models.Model):
                               default='draft')
     objects = models.Manager()  # The default manager
     published = PublishManager()  # our custom manager
+    tags = TaggableManager()
 
     # canonical
     def get_absolute_url(self):
@@ -50,3 +52,20 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
