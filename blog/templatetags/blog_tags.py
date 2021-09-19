f0bd35c7879193e74bc77f7c80b64480d4ca3453
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2021-09-19 07:24:41 -0700

    completed adding custom filter. adding sitemap feature to program

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,10 +1,13 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
 
+# creating custom tags
 @register.simple_tag
 def total_posts():
     return Post.published.count()
@@ -18,4 +21,10 @@ def show_latest_posts(count=5):
 
 @register.simple_tag
 def get_most_commented_posts(count=5):
-    return Post.published.annotate(total_comments = Count('comments')).order_by('-total_comments')[:count]
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+# creating custom filters
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
